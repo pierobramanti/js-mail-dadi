3f50e71d4a96e5397e0ecb9289115f57701891f9
pierobramanti <piero.bramanti94@gmail.com>
2024-06-28 15:47:25 +0200

    [UPD] deleted input in html and deleted const input and container

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,22 +1,7 @@
 // CREAZIONE ARRAY EMAIL//
 const mailList =[ "alessandro.melillo@gmail.com", "andrea.gasperini@gmail.com", "arturo.agostinelli@gmail.com", "cecilia.angelotti@gmail.com", "claudia.pellegrino@gmail.com", "sara.melle@gmail.com"]
-
-// CREAZIONE INPUT PER INSERIMENTO EMAIL//
-let userInput = document.querySelector(".mail-input").toLowerCase()
-console.log(userInput)
+console.log(mailList)
 
 
-// DICHIARO COSTANTE CONTAINER E BTN //
-const container = document.querySelector(".container");
-
-const btn = document.querySelector(".input-button");
 
 
-//
-let authorization = false;
-
-for (let i = 0; i < mailList.length; i++) {
-    if (mailList[i] === userInput) {
-        authorization = true;
-    }
-}
