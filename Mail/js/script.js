6bdd0442ab41f375e17e7f49c5b47a67cf3011ae
pierobramanti <piero.bramanti94@gmail.com>
2024-06-28 15:14:33 +0200

    [UPD] created email's array

---
+++ Mail/js/script.js
@@ -0,0 +1,2 @@
+// CREAZIONE ARRAY EMAIL//
+const mailList =[ "alessandro.melillo@gmail.com", "andrea.gasperini@gmail.com", "arturo.agostinelli@gmail.com", "cecilia.angelotti@gmail.com", "claudia.pellegrino@gmail.com", "sara.melle@gmail.com"]
