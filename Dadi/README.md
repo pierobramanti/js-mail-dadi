79fe37331e47ab82be939753bfefe933f9d47ec0
pierobramanti <piero.bramanti94@gmail.com>
2024-06-28 17:49:16 +0200

    [UPD] declarated variables

---
+++ Dadi/README.md
@@ -0,0 +1,6 @@
+SCOMPOSIZIONE ESERCIZIO
+
+1 dichiaro la variabile del pulsante
+2 inserisco la funzione
+3 dichiaro la variabile player
+4 dichiaro la variabile pcPlayer
---
+++ Dadi/js/script.js
@@ -0,0 +1,13 @@
+//INSERISCO IL PULSANTE //
+let play = document.getElementById("play")
+
+// INSERISCO FUNZIONE
+play.addEventListener("click", function() {
+// DICHIARO VARIABILE
+    let player = Math.floor(Math.random() * 6 + 1);
+// DICHIARO VARIABILE PC
+    let pcPlayer = Math.floor(Math.random() * 6 + 1);
+
+})
+
+
