6a5af50fab4ec6934ae42dfa32893e5e745aced6
pierobramanti <piero.bramanti94@gmail.com>
2024-06-28 18:01:06 +0200

    [UPD] inserted if

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -8,6 +8,18 @@ play.addEventListener("click", function() {
 // DICHIARO VARIABILE PC
     let pcPlayer = Math.floor(Math.random() * 6 + 1);
 
+// INSERISCO IF
+if (player > pcPlayer) {
+    console.log(`il giocatore 1 ha vinto con un lancio del valore di ${player}, battendo il lancio del valore di ${pcPlayer} del computer`)
+}
+// INSERISCO ELSE IF
+else if (player === pcPlayer) {
+    console.log(`Pareggio entrambi avete ottenuto un ${player}`)
+}
+// INSERISCO ELSE
+else {
+    console.log(`Il computer ha vinto con un lancio del valore di ${pcPlayer}, battendo il lancio ${player} del giocatore 1`)
+}
 })
 
 
