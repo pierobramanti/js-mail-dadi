15f89d8f5ce41bc0be50a03d233db199ce830ce1
pierobramanti <piero.bramanti94@gmail.com>
2024-06-28 15:51:30 +0200

    [UPD] declarated variable user and  added prompt

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -2,6 +2,7 @@
 const mailList =[ "alessandro.melillo@gmail.com", "andrea.gasperini@gmail.com", "arturo.agostinelli@gmail.com", "cecilia.angelotti@gmail.com", "claudia.pellegrino@gmail.com", "sara.melle@gmail.com"]
 console.log(mailList)
 
-
+// CREAZIONE VARIABILE USER E ASSEGNAZIONE VALORE  UGUALE A PROMPT//
+let user = prompt("inserisci la tua email").toLowerCase()
 
 
